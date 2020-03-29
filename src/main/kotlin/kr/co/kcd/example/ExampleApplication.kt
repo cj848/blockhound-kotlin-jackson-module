394c8baad5fc7ffb97e71a467296c4334b7e51e9
kaka <kaka@kcd.co.kr>
2020-03-30 06:48:24 +0900

    change application class

--- src/main/kotlin/kr/co/kcd/example/ExampleApplication.kt
+++ src/main/kotlin/kr/co/kcd/example/ExampleApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class ChickenApplication
+class ExampleApplication
 
 fun main(args: Array<String>) {
-    runApplication<ChickenApplication>(*args)
+    runApplication<ExampleApplication>(*args)
 }
